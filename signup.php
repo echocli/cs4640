3397b32b0bd5021c7188ca2161e7fd16115f56d8
Echo Li <el7ne@virginia.edu>
2018-02-27 02:01:39 -0500

    attempting jquery form validation on signup.php

--- signup.php
+++ signup.php
@@ -74,16 +74,60 @@
 
 		</div>
 		<!-- END: header -->
+	<script src="js/jquery.js"></script>
+	<script src="js/jquery.validate.js"></script>
+	<script>
+	$(document).ready(function(){
+		$('#firstname').on('input', function() {
+			var input=$(this);
+			var re = /^[a-zA-Z]+$/;
+			var is_firstname=re.test(input.val());
+			if(is_firstname){input.removeClass("invalid").addClass("valid");}
+			else{input.removeClass("valid").addClass("invalid");}
+		});
+
+		$("#signupform").validate({
+		  rules: {
+		    firstname: {
+		    	required: true,
+		      	email: true
+		    }
+		    email: {
+		      	required: true,
+		      	email: true
+		    }
+		  },
+		  messages: {
+		    firstname: "Enter your firstname",
+			lastname: "Enter your lastname",
+			username: {
+					required: "Enter a username",
+					minlength: jQuery.validator.format("Enter at least {0} characters"),
+					remote: jQuery.validator.format("{0} is already in use")
+			},
+			email: {
+					required: "Please enter a valid email address",
+					minlength: "Please enter a valid email address",
+					remote: jQuery.validator.format("{0} is already in use")
+			}
+		  },
+		  errorPlacement: function(error, element) {
+				error.insertBefore(element);
+			}
+		});
+	});
+	</script>
 
 		<div id="fh5co-contact" class="animate-box">
 
 			<div class="container">
-				<form action="#">
+				<form action="signup.php" method="post" name="signupform">
 					<div class="row">
 						<div class="col-md-6">
 							<div class="form-group">
 								<label>First name</label>
-								<input type="text" class="form-control" required autocomplete="on" name="firstname" id="firstname" maxlength="50" />
+								<input type="text" class="form-control" required autocomplete="on" name="firstname" id="firstname" maxlength="50" pattern="[A-Za-z]" title="Name must only be letters"/>
+								<span class="error">Field must only be letters</span>
 							</div>
 						</div>
 						<div class="col-md-6">
@@ -96,14 +140,14 @@
 					<div class="row">
 						<div class="col-md-6">
 							<div class="form-group">
-								<label>username</label>
+								<label>Username</label>
 								<input type="text" class="form-control" required autocomplete="on" name="username" id="username" maxlength="50" />
 							</div>
 						</div>
 						<div class="col-md-6">
 							<div class="form-group">
 								<label>Email</label>	
-								<input type="email" class="form-control" required autocomplete="on" name="email" id="email" maxlength="50"/>
+								<input type="email" class="form-control" name="email" id="email" maxlength="50"/>
 							</div>
 						</div>
 					</div>
@@ -127,6 +171,20 @@
                     				</select>
                     			</div>
 
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Password</label>
+								<input type="password" class="form-control" required name="password" id="password" minlength="8"/>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group">
+								<label>Confirm password</label>	
+								<input type="password" class="form-control" required name="confirm_password" id="confirm_password" minlength="8" />
 							</div>
 						</div>
 					</div>
