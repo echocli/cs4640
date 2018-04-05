88f873e9125c86ddfe49843b1e681f6c290191d3
Echo Li <el7ne@virginia.edu>
2018-04-05 18:42:08 -0400

    test

--- logout.php
+++
@@ -1,8 +0,0 @@
-<script type="text/javascript">
-window.onload = function(){
-  document.forms['Logout'].submit();
-}
-</script>
-<form hidden action="/examples/servlets/servlet/LogoutController" method="get" name="Logout">
-	<input type="submit" value="Logout">
-</form>
